be9b6049fa3f1b9e7a45fd13e059d4041c0bde35
richard <richarentenza@gmail.com>
2025-02-22 13:03:44 -0800

    Fix for customer configurations

--- CryptoInvestment/ViewModels/CustomerConfiguration/WithdrawalsPasswordViewModel.cs
+++ CryptoInvestment/ViewModels/CustomerConfiguration/WithdrawalsPasswordViewModel.cs
@@ -16,4 +16,28 @@ public class WithdrawalsPasswordViewModel
     public string Character5 { get; set; } = null!;
     [Required(ErrorMessage = "La contraseña para retiros es inválida")]
     public string Character6 { get; set; } = null!;
+    
+    [Compare("Character1", ErrorMessage = "Las contraseñas no coinciden")]
+    [Required(ErrorMessage = "La contraseña para retiros es inválida")]
+    public string ConfirmCharacter1 { get; set; } = null!;
+    
+    [Compare("Character2", ErrorMessage = "Las contraseñas no coinciden")]
+    [Required(ErrorMessage = "La contraseña para retiros es inválida")]
+    public string ConfirmCharacter2 { get; set; } = null!;
+    
+    [Compare("Character3", ErrorMessage = "Las contraseñas no coinciden")]
+    [Required(ErrorMessage = "La contraseña para retiros es inválida")]
+    public string ConfirmCharacter3 { get; set; } = null!;
+    
+    [Compare("Character4", ErrorMessage = "Las contraseñas no coinciden")]
+    [Required(ErrorMessage = "La contraseña para retiros es inválida")]
+    public string ConfirmCharacter4 { get; set; } = null!;
+    
+    [Compare("Character5", ErrorMessage = "Las contraseñas no coinciden")]
+    [Required(ErrorMessage = "La contraseña para retiros es inválida")]
+    public string ConfirmCharacter5 { get; set; } = null!;
+    
+    [Compare("Character6", ErrorMessage = "Las contraseñas no coinciden")]
+    [Required(ErrorMessage = "La contraseña para retiros es inválida")]
+    public string ConfirmCharacter6 { get; set; } = null!;
 }
